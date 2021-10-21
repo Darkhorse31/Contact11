291bd806bbc4a096cb87dcdb9d152be7290f780d
Darkhorse31 <81240621+Darkhorse31@users.noreply.github.com>
2021-10-21 11:09:26 +0530

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,11 @@ function App(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className=".circle-img" src={props.img} alt="avatar_img" />
+          <img className="circle-img" src={props.img} alt="avatar_img" />
         </div>
         <div className="bottom">
-          <p>{props.phone}</p>
-          <p>{props.email}</p>
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
     </div>
